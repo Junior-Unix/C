f7a5cf50034959858a5267e168a15afda9b619c5
Junior-Unix <sonjunior@live.com>
2021-06-27 14:08:33 -0300

    commited

--- aula18media.c
+++ aula18media.c
@@ -6,24 +6,40 @@ int main()
 {
 
 
-	float nota1, nota2, nota3, nota4, divisor, media;
-	nota1 = nota2 = nota3 = nota4 = divisor = media = 0;
+	float nota1, nota2, nota3, nota4, divisor, resultadoSoma, media;
+//	nota1 = nota2 = nota3 = nota4 = divisor = media;
+	nota1 = 0;
+	nota2 = 0;
+	nota3 = 0;
+	nota4 = 0;
+	divisor = 0;
+	resultadoSoma = 0;
+	media = 0;
 
 	printf("==============Media==============\n\n");
 
 	printf("Digite os valores em sequência:");
-	printf("\nPrimeiro valor\n");
-	scanf("%f\n", &nota1);
-	printf("\nSegundo valor\n");
-	scanf("%f\n", &nota2);
-	printf("\nTerceiro valor\n");
-	scanf("%f\n", &nota3);
-	printf("\nQuarto valor\n");
-	scanf("%f\n", &nota4);
-	printf("qual o divisor?");
-	scanf("%f\n", &divisor);
+	printf("Primeiro valor\n");
+	scanf("%f", &nota1);
 
-	media = (nota1 + nota2 + nota3 + nota4) /  divisor /*4*/;
+	printf("Segundo valor\n");
+	scanf("%f", &nota2);
+
+	printf("Terceiro valor\n");
+	scanf("%f", &nota3);
+
+	printf("Quarto valor\n");
+	scanf("%f", &nota4);
+
+	resultadoSoma = (nota1 + nota2 + nota3 + nota4);
+
+	printf("Resultado da soma dos quatro valores eh: %f", resultadoSoma);
+
+	printf("qual o divisor?");
+	scanf("%f", &divisor);
+
+
+	media = (resultadoSoma /  divisor) /*4*/;
 
 	printf("A media entre os valores eh: %f", media);
 
