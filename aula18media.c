265a1f7047c1a171a50cbee7c474f04605e8cc11
Junior-Unix <sonjunior@live.com>
2021-06-27 17:30:48 -0300

    commited

--- aula18media.c
+++ aula18media.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 
 
-int main()
+long int main()
 {
 
+	printf("================== Media 1.0 =====================\n\a");
+	printf("==================NF Tecnologia ==================\n\n");
 
 	float nota1, nota2, nota3, nota4, divisor, resultadoSoma, media;
 //	nota1 = nota2 = nota3 = nota4 = divisor = media;
@@ -16,32 +18,63 @@ int main()
 	resultadoSoma = 0;
 	media = 0;
 
-	printf("==============Media==============\n\n");
 
-	printf("Digite os valores em sequência:");
-	printf("Primeiro valor\n");
-	scanf("%f", &nota1);
+//	system (clear);
 
-	printf("Segundo valor\n");
-	scanf("%f", &nota2);
+//	printf("==============Media==============\n\n");
 
-	printf("Terceiro valor\n");
-	scanf("%f", &nota3);
+	printf("Digite os valores em sequência:\n");
+	printf("->Primeiro valor:\n");
+	scanf("%f:", &nota1);
 
-	printf("Quarto valor\n");
-	scanf("%f", &nota4);
+	if(nota1 > 100)
+	{
+		printf("\nAtenção! Apenas numeros até 100.\n");
+
+	}
+
+	for(nota1 >= 1; nota1 <=100; nota1++){
+
+		printf("%f", nota1);
+	}
+	system("break");
+
+	printf("->Segundo valor:\n");
+	scanf("%f:", &nota2);
+
+	printf("->Terceiro valor:\n");
+	scanf("%f:", &nota3);
+
+	printf("->Quarto valor:\n");
+	scanf("%f:", &nota4);
 
 	resultadoSoma = (nota1 + nota2 + nota3 + nota4);
 
-	printf("Resultado da soma dos quatro valores eh: %f", resultadoSoma);
+	printf("\n\a->Resultado da soma dos quatro valores eh:\n>%f<\n\a", resultadoSoma);
 
-	printf("qual o divisor?");
+	printf("\n\n->Qual o divisor?\n");
 	scanf("%f", &divisor);
 
 
 	media = (resultadoSoma /  divisor) /*4*/;
 
-	printf("A media entre os valores eh: %f", media);
+	if(media > 40)
+        {
+
+                printf("Valor da media acima de 40\n");
+        }else{
+
+                printf("Valor da media baixo de 40\n");
+
+        }
+
+
+
+	printf("A media entre os valores eh: %f\n\a", media);
+
+
+
+	printf("--->Sizeof = %ld\n\n", sizeof(float)); //sizeof éh o tamanho da variant em bytes.
 
 
 	return 0;
