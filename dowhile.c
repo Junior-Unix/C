0703671c20600ca29f14dca0cab37b9472c978b6
Junior-Unix <sonjunior@live.com>
2021-07-04 12:32:25 -0300

    commited

--- dowhile.c
+++ dowhile.c
@@ -3,6 +3,8 @@
 #include <ctype.h>
 
 signed long int main()
+
+
 {
 
 
@@ -12,6 +14,19 @@ signed long int main()
 //contador de 0 a 100.
 int cont = 0;
 int valor = 100;
+char sim;
+do{
+    //cont +=1;
+    
+    system("clear");
+    
+    printf("Digite <Y> pra continuar!\n");
+    //printf("%i de vezes repetiu a instrução\n", cont);
+    sim = getchar();
+}while(sim != 'Y');
+
+
+
 
 while( cont < valor){
     
@@ -22,56 +37,61 @@ while( cont < valor){
 
 
 
-system("neofetch");
+system("ufetch");
 
-
-signed long int a, b, c, d, x, media;
+float a, b, c, d, x, media;
     a = b = c = d = x = media = 0;
-    signed long int valorMinimo, valorMaximo;
+float valorMinimo, valorMaximo;
     valorMinimo = valorMaximo = 0;
 
     printf("\nDigite o valor mínimo:\n");
-        scanf("%li", &valorMinimo);
+        scanf("%f", &valorMinimo);
+        char confirme;
+        do
+        {
+            printf("Se o valor é esse %f confirme com Y maiúsculo, se não for digite n!: \n", valorMinimo);
+            confirme = getchar();
+        }while (confirme != 'Y');
+
     printf("Digite o valor máximo:\n");
-        scanf("%li", &valorMaximo);
+        scanf("%f", &valorMaximo);
     
     
     printf("Digite o primeiro valor:\n");
-    scanf("%li", &a);
+    scanf("%f", &a);
 
     system("clear");
     if( a < valorMinimo){
         printf("\nValor nulo ou abaixo do valor minimo!\n");    
-
         
     }else{
         if( a > valorMaximo)
         {
-            printf("Valor maior que valor máximo!\n");
+            printf("Valor maior que valor estipulado como máximo!\n");
         }else{
             ((a > valorMinimo) && ( a <= valorMaximo));
-            printf("\n\nValor digitado > %li < está entre  valor minimo > %li < e o valor máximo > %li <!\n\n\n", a, valorMinimo, valorMaximo);
+            printf("\n\nValor digitado > %f < está entre  valor minimo > %f < e o valor máximo > %f <!\n\n\n", a, valorMinimo, valorMaximo);
         }
     }
 
     while(( a < valorMinimo) || ( a > valorMaximo))
     {
          
-        printf("Digite novamente!\n");
-        scanf("%li", &a);
+        printf(" Se não, digite novamente!\n");
+        scanf("%f", &a);
         if((a > valorMinimo) && ( a <= valorMaximo))
         {
-            printf("\n\nValor digitado > %li < está entre  valor minimo > %li < e o valor máximo > %li < e foi atribuida a letra 'a'!\n\n\n", a, valorMinimo, valorMaximo);
+            printf("\n\nValor digitado > %f < está entre  valor minimo > %f < e o valor máximo > %f < e foi atribuida a letra 'a'!\n\n\n", a, valorMinimo, valorMaximo);
         }
     }
 
-printf("<a=%li>\n\n", a);
+printf("<a=%f>\n\n", a);
 
 
 
 
 
-    printf("Sizeof primeiro numero > %li bytes <!\n\n\n\a", sizeof(a));
+    printf("Sizeof primeiro numero > %f bytes <!\n\n\n\a", sizeof(a));
 
     return 0 ;
 }
