0828ba76360fdc16a8e795ff930a007c073f1f96
Junior-Unix <sonjunior@live.com>
2021-07-31 18:06:30 -0300

    commited

--- exerc7-5.c
+++ exerc7-5.c
@@ -1,24 +1,36 @@
-/* Exercício 7 seção 5.: Faça um programa que leia 10 inteiros positivos, ignorando não positivos,
+/* Exercício 7 seção 5.: Faça um programa que leia N inteiros positivos, ignorando não positivos,
 e imprima sua média.
  */
 
 #include <stdio.h>
+// #define N 10
+
 
 int main(void){
 
-    float numero[10];
+  int somaZ;
+  float soma;
+  
+  
+    printf("Quantas notas serão digitadas?\n:");
+    scanf("%i", &somaZ);
+    float num[somaZ];
     
-    
-
-    printf("Digite 10 valores.\n");
-
-    for( float i = 0; i < 10; i++){
-        scanf("%f", &numero);
-        printf("%f\n", numero[i]);
+    printf("Informe os %i valores:\n", somaZ);
+        for(int i = 0; i < somaZ; i++){
+            scanf("%f", &num[i]);
+            soma = soma + num[i];
         }
-    // for( int i = 0; i < 10; i++){
-    \
-    }
+        
 
+        
+        for( int i = 0; i < somaZ; i++){
+            printf("a nota %i é [ %.1f ]\n",i + 1, num[i]);
+        }
+           
+        printf("A soma é: [%.1f].\n", soma);
+        printf("E a média é: [%.1f].\n", soma / somaZ);  
+            
+    
     return 0 ;
 }
---
+++ numerosOrdem.c
@@ -0,0 +1,42 @@
+// Ordem crescente ou decrescente:
+#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+
+int main(void){
+
+    short int num[ ] = { }, i, x, temp, valor, aux = 0;
+
+    setlocale( LC_ALL, "portuguese" );
+
+    do{
+        printf("Insira os valores: \n");
+        scanf("%hi", &valor);
+
+        num[aux] = valor;
+        aux++;
+
+
+    }while( valor != 0 );
+
+    for( i = 0; i < aux; i++){
+        for( x = i + 1; x < aux; x++){
+            if( num[i] > num[x] ){
+            
+            
+            temp = num[i];
+            num[i] = num[x];
+            num[x] = temp;
+           }
+        }
+    }
+
+    printf("A ordem crescente da numeração adicionada é: \n");
+
+        for ( i = 0; i < aux; i++){
+            printf("%hi", num[i]);
+            
+        }
+
+    return 0 ;
+}
