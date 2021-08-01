83af5f4b27ee24cb58b8d7ea17c34765eb4b69d5
Junior-Unix <sonjunior@live.com>
2021-08-01 20:16:13 -0300

    com

---
+++ treino.c
@@ -0,0 +1,29 @@
+/* Treino */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int inteiros, media = 0;
+    
+
+    for(int i=0;i<10;i++){
+        scanf("%d",&inteiros);
+        media = media+inteiros;
+    }
+    printf("Média  %d\n\n", media / 10);
+    /* for(int i=0;i<10;i++){
+        printf("%d\n\n",inteiros);
+    }
+ */
+
+
+
+
+
+
+
+
+    return 0 ;
+}
