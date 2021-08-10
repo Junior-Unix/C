e1b0633799e611b7f1bb3977c0e3f8a7d2cdbab3
Junior-Unix <sonjunior@live.com>
2021-08-10 16:10:15 -0300

    Exercício 22 seção 5.

--- exerc22-5.c
+++ exerc22-5.c
@@ -1,45 +1,34 @@
 /* Escreva um programa completo que permita a qualquer aluno introduzir, pelo teclado, uma sequência
- * arbitrária de notas( válidas no intervalo de 10 a 20 ) e que mostre na tela, como resultado, a 
+ * arbitrária de notas( válidas no intervalo de 10 a 20 ) e que mostre na tela, como resultado, a
  * correspondente média aritmética. O número de notas com que o aluno pretende efetuar o cálculo
- * não será fornecido ao programa, o qual terminará quando for introduzido um valor que não seja 
+ * não será fornecido ao programa, o qual terminará quando for introduzido um valor que não seja
  * váido como nota de aprovação
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
+int main(void){
 
-int main(){
-
-int notas[0]={};
+int nota;
+int  soma = 0;
 int i = 0;
 do{
-	scanf("%d", &notas[i] );
-	i = i + 1 ;
+	
+	printf("Informe a nota.\n");
+	scanf("%d", &nota);
+	if(nota >=10 && nota <= 20 ){
+	soma = soma + nota;
+	i++;
+	
+	}
+	}while( nota >=10 && nota <= 20 );
 
-
-
-
-}while( notas[i] >= 10 && notas[i] <= 20 );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	float media = soma / i ;
+	printf("[%f ]\n", media);
+	
 
 	return 0 ;
 
-
-
 }
+
