3a5560d876ab08f09b8494a261fd5545a65e12d4
Junior-Unix <sonjunior@live.com>
2021-08-11 21:35:08 -0300

    Exercício 24 seção 5.

--- exerc24-5.c
+++ exerc24-5.c
@@ -1,48 +1,30 @@
-/* Escreva um programa que leia um numero inteiro e calcule a soma de todos os divisores desse n�mero, com exe��o dele
+/* Escreva um programa que leia um numero inteiro e calcule a soma de todos os divisores desse nmero, 
+ * com exeo dele
  * próprio. Ex.: A soma dos divisores do número 66 é 1 + 2 + 3 + 6 + 11 + 22 + 33 = 78.
  */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <locale.h>
-
-
-int linha( int num ){
-	int i;
-	for( i = 1; i <= num; i++ )
-	putchar('*');
-	putchar('\n');
-}
-
-
-
 int main(){
 
-setlocale ( LC_ALL, "portuguese");
-linha(77);
-
 int N;
-int somai;
-int i;
-int divisor;
 
-printf("Informe um numero inteiro para calcular a soma dos divisores.\n");
-scanf("%i", &N );
+	printf("Informe um numero inteiro para calcular a soma dos \ndivisores, menos ele prprio.\n");
+	scanf("%i", &N );
+	
 
-	do{
-		for( i = N; i >= 0; i-- ){
-			divisor = N % i;
-			if( divisor == 0 ){
-				printf("[%i ]\n", i);
-				somai = somai + i;
-			}
+int res = 0;
+	for ( int i = 1; i < N; i++){
+		if( N % i == 0 ){
+		res = res + i;
+		printf("%i\n", i);
+		
 		}
-	}while( i >= 0 );
+	}
 
-printf("[%i ]", somai);
-
+	printf("A soma dos divisores de [%i ] excluindo ele mesmo eh: [%i ]", N, res);
 
 
 	return 0 ;
+
 }
