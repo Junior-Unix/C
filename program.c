cf19e98a7ca0dd6b2a8e0cfaf1d1b4901f6c565a
Nelson Junior <sonjunior@live.com>
2024-07-17 15:49:47 +0000

    scanf

--- program.c
+++ program.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
 void main(){
+
+    int snum;
+    printf("Digite um valor: ");
+    scanf("%d", &snum);
+    printf("O valor foi %d\n", snum);
+
+
+    int num;
+    num = -17;
+    int a = 10, b, c = -123, d;
+
+    int val = num;
+
+    printf("\t%d %d\n",num, num+1);
+
     printf("C\nis \"the\" greatest\nLanguage!\n");
 }
