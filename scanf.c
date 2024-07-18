11c2ee0361a27222a186ba5efcbc84d4e4014c4d
Nelson Junior <sonjunior@live.com>
2024-07-18 23:33:06 +0000

    scanf.

---
+++ scanf.c
@@ -0,0 +1,13 @@
+#include <stdint.h>
+
+void main() {
+
+    char ch;
+
+    printf("Introduza um Caracter: ");
+    scanf("%c", &ch);
+
+    printf("O caractere introduzido foi '%c'\n", ch);
+
+    return 0;
+}
