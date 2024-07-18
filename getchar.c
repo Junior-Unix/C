3c61826436e743c22f95637d62a0e50e3c61d49d
Nelson Junior <sonjunior@live.com>
2024-07-18 23:29:42 +0000

    getchar.

---
+++ getchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main() {
+    char ch;
+    printf("Introduza um Caractere: ");
+    ch = getchar();
+    printf("O caracter introduzido foi '%c'\n", ch);
+
+    return 0;
+}
