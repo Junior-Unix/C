8f166178bb87087e471114727d09735f2b27e129
Nelson Junior <sonjunior@live.com>
2024-07-22 01:37:45 +0000

    eh dígito ou não, perfumaria.

--- ehDigitoOuNao.c
+++ ehDigitoOuNao.c
@@ -3,20 +3,40 @@
 
 #include <stdio.h>
 
+char linha();
 int x_isDigit(char ch);
 
 int main(){
 
     char ch;
-
+    
 //    printf("Introduza um caractere ou CTRL-C para finalizar:");
-    while(1){/*Termina com CTRL-C*/
-        ch=getchar();
-        if(!x_isDigit(ch))/*se não for digito*/
-            putchar(ch);
-    }
+    do{/*Termina com CTRL-C*/
+        
+        printf("Introduza um caractere ou CTRL-C para finalizar: ");
+        scanf(" %c", &ch);
+//        ch=getchar();
+            if(!x_isDigit(ch)){/*se não for digito*/
+                linha();
+                printf("*( %c ), não é um dígito. *\n", ch);
+                linha();
+            }else
+                if(x_isDigit(ch)){
+                    linha();
+                    printf("*( %c ), é um dígito.     *\n", ch);
+                    linha();
+                }
+//            putchar(ch);
+    }while(1);
 
     return 0;
+}
+
+char linha(){
+    int i;
+    for(i=0; i<=25; i++)
+        putchar('*');
+    putchar('\n');
 }
 
 int x_isDigit(char ch){
