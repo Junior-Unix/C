476e19eb24d9d4eead4997cafaf37e040c02a7d6
Nelson Junior <sonjunior@live.com>
2024-07-22 18:49:10 +0000

    Refatoraçao, potencia.c

--- potencia.c
+++ potencia.c
@@ -3,39 +3,39 @@
 #include <stdio.h>
 
 float baseExpoente(float base, int expoente);
-float Pot(float x, int n);
+float Pot(float base, int expoente);
 
 int main(){
     float base;
     int expoente;
+baseExpoente(base, expoente);
+    // printf("Digite:");
+    // scanf("%f", &base);
+    // printf("Digite:");
+    // scanf("%d", &expoente);
+//Pot(base, expoente);
 
-    printf("Digite:");
-    scanf("%f", &base);
-    printf("Digite:");
-    scanf("%d", &expoente);
-Pot(base, expoente);
-
-    printf("%f\n", Pot(base, expoente));
+//    printf("%f\n", baseExpoente(base, expoente));
 
     return 0;
 }
 
 float baseExpoente(float base, int expoente){
-    while(1){
-        printf("Digite:");
+    while(1){ /*Termina com CTRL-C*/
+        printf("Digite a BASE ou CTRL-C para terminar: ");
         scanf("%f", &base);
-        printf("Digite:");
+        printf("Digite o EXPOENTE ou CTRL-C para terminar: ");
         scanf("%d", &expoente);
         
-        Pot(base, expoente);
+        printf("(--> %.2f^%d = %.2f)\n", base, expoente, Pot(base, expoente));
     }
 }
 
-float Pot(float x, int n){
+float Pot(float base, int expoente){
     float res;
     int i;
 
-    for(i=1, res=1.0; i<=n; i++)
-        res*=x; /*res=res*x;*/
+    for(i=1, res=1.0; i<=expoente; i++)
+        res*=base; /*res=res*x;*/
     return res;
 }
