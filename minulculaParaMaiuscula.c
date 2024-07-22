89a5ce2bec780eb13de698fc9d70f04a09d977ad
Nelson Junior <sonjunior@live.com>
2024-07-22 01:57:38 +0000

    melhoramentos com erros no código minúscula para maiuscula.

--- minulculaParaMaiuscula.c
+++ minulculaParaMaiuscula.c
@@ -1,19 +1,24 @@
 /*Transforme qualquer caractere na sua maiúscula correspondente*/
 #include <stdio.h>
 
-int x_ToUpper(char ch){
-    if(ch>='a' && ch<='z')/*Se minúscula*/
-        return ch + 'A'-'a';
-    else
-        return ch;/*Devolve a própria*/
-}
+int x_ToUpper(char ch);
 
 int main(){
 
     char ch;
 
+    printf("Introduza uma letra: \n");
+    
     while(1){/*Termina com CTRL-C*/
+        printf("Letra:");
         ch=getchar();
             putchar(x_ToUpper(ch));
     }
+}
+
+int x_ToUpper(char ch){
+    if(ch>='a' && ch<='z')/*Se minúscula*/
+        return ch + 'A'-'a';
+    else
+        return ch;/*Devolve a própria*/
 }
