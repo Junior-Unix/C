b09a2308fcd1222ad22ced29b271e11f939e3493
Nelson Junior <sonjunior@live.com>
2024-07-22 16:57:39 +0000

    refatoração.

--- potencia.c
+++ potencia.c
@@ -2,18 +2,19 @@
 
 #include <stdio.h>
 
+float Pot(float x, int n);
+
+int main(){
+    printf("%f %f %f\n", Pot(2.0, 4), Pot(1.234, 4), Pot(3.0, 0));
+
+    return 0;
+}
+
 float Pot(float x, int n){
     float res;
     int i;
 
     for(i=1, res=1.0; i<=n; i++)
         res*=x; /*res=res*x;*/
-
     return res;
-}
-
-int main(){
-    printf("%f %f %f\n", Pot(2.0, 4), Pot(1.234, 4), Pot(3.0, 0));
-
-    return 0;
 }
