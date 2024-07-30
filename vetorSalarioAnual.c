50e532eb5dc8d93c18dbbc759cd4aa3f9a823840
Junior-Unix <sonjunior@live.com>
2024-07-30 01:21:37 -0300

    Refatoração cabeçalho ¬> função insereImprime(float salario[12]);

--- vetorSalarioAnual.c
+++ vetorSalarioAnual.c
@@ -9,64 +9,42 @@ char imprimirSalarioAnual(float salario[12]);
 char insereImprime(float salario[12]);
 
 int main(int){
-    float salario[12]; /*12 meses**/
+    float salario[12]; /*12 meses*/
+    int i;
 
-    // inserirSalario(salario); 
-    // imprimirSalarioAnual(salario);
-    insereImprime(salario);
+    inserirSalario(salario); 
+    imprimirSalarioAnual(salario);
 
 
 
     return 0;
 }
 
-// void inserirSalario(float salario[12]){
-//     int i;
-//     for(i=0; i<12; i++){
-//         printf("Insira o salário do mês %d:", i+1);
-//         scanf("%f", &salario[i]);
-//     }
-// }
+void inserirSalario(float salario[12]){
+    int i;
+    for(i=0; i<12; i++){
+        printf("Insira o salário do mês %d:", i+1);
+        scanf("%f", &salario[i]);
+    }
+}
 
-// /*Mostrar os valores mensais e calcular o total*/
-// char imprimirSalarioAnual(float salario[12]){
+/*Mostrar os valores mensais e calcular o total*/
+char imprimirSalarioAnual(float salario[12]){
 
-//     float total;
-//     int i;
+    float total;
+    int i;
 
-//     puts("Mês Valor ");
-//     for(i=0, total=0.0; i<12; i++){
-//     printf("%3d %9.2f\n", i+1, salario[i]);
-//     total+=salario[i];
-//     }
+    puts("Mês Valor ");
+    for(i=0, total=0.0; i<12; i++){
+    printf("%3d %9.2f\n", i+1, salario[i]);
+    total+=salario[i];
+    }
 
-//     printf("Total anual: %9.2f\n", total);
+    printf("Total anual: %9.2f\n", total);
 
-// }
+}
 
 char insereImprime(float salario[12]){
-
-        void inserirSalario(float salario[12]){
-        int i;
-        for(i=0; i<12; i++){
-            printf("Insira o salário do mês %d:", i+1);
-            scanf("%f", &salario[i]);
-        }
-    }
-
-    /*Mostrar os valores mensais e calcular o total*/
-    char imprimirSalarioAnual(float salario[12]){
-
-        float total;
-        int i;
-
-        puts("Mês Valor ");
-        for(i=0, total=0.0; i<12; i++){
-        printf("%3d %9.2f\n", i+1, salario[i]);
-        total+=salario[i];
-        }
-
-        printf("Total anual: %9.2f\n", total);
-
-    }
+    insereImprime(salario);
+    imprimirSalarioAnual(salario);
 }
