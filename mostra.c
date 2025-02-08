64c45748149d0253dfd656177fbe10e8a17db31d
Junior-Unix <sonjunior@live.com>
2025-02-08 16:28:39 -0300

    mostra.c.

---
+++ mostra.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define OP_OUT "-o"
+
+main(int argc, char *argv[]){
+	
+	FILE *fin, *fout=stdout;
+	int i; /*Para percoeere todos os parêmetros*/
+	int ch; /*Para ler os caracteres do arquivo*/
+	char opcao = 'M'; /*Por padrão -M*/
+
+	/*Verificar se o último parâmetro é do tipo -o Ar1. */
+
+	if(strnicmp(argv[argc-1],OP_OUT,strlen(OP_OUT))==0){
+		if((fout=fopen(argv[argc-1]+2,"wt"))==NULL){
+			fprintf(stderr,"Impossível criar o arquivo %s\n\n", argv[argc-1]);
+			exit(1);
+		}	
+		else
+			argc--; /*O último parâmetro já foi tratado.*/
+	}
+
+	for(i=1;i<argc;i++){
+		if(argv[i][0]=='-') /*Se for uma opção.*/
+			switch(argv[i][1]){
+				case 'm':
+				case 'M':
+				case 'd':
+				case 'D':
+					opcao=argv[i][1]; /*Se for qualquer outra opção -- Ignorar.*/
+			}
+		else /*E um arquivo.*/{
+			fprintf(fout,"%s\n",argv[i]);
+			if((fin=fopen(argv[i],"r"))NULL)
+				continue; /*Passa ao próximo.*/
+			while((ch=fgetc(fin))!=EOF)
+				switch(opcao){
+					case 'm':
+						if(islower(ch))
+							fputc(ch,fout);
+						break;
+					case 'M':
+						if(isupper(ch))
+							fputc(ch,fout);
+						break;
+					case 'd':
+					case 'D':
+						if(isdigit(ch))
+							fputc(ch,fout);
+						break;
+				}
+			fclose(fin);
+			fputc('\n',fout); /*Mudar de linha*/
+		}
+		fclose(fout);
+			exit(0);
+
+}
