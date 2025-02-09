413e3cdf1ae487c25e9ce35f7ec87b7215971eed
Junior-Unix <sonjunior@live.com>
2025-02-09 14:37:06 -0300

    struct function.

---
+++ prog1101_ExtructFunction.c
@@ -0,0 +1,38 @@
+/*Função que permite escrever na tela    *
+ * os valores existentes em uma estrutura*
+ *recebida como argumento                *
+ *****************************************/
+
+
+#include <stdio.h>
+
+typedef struct{
+	int Dia,
+	Mes,
+	Ano;
+}DATA;
+
+typedef struct pessoa{
+	char Nome[100];
+	int Idade;
+	float Salario;
+	DATA Nasc;
+}PESSOA;
+
+void Mostrar(x){
+	printf("Nome: %s\n",x.Nome);
+	printf("Idade: %d\n",x.Idade);
+	printf("Salário: %.2f\n",x.Salario);
+	printf("Data Nascimento: %d/%d/%d\n",x.Nasc.Dia,x.Nasc.Mes,x.Nasc.Ano);
+}
+
+main(){
+	
+	struct pessoa p = {"Carlos",23,12345.67,{23,5,1954}};
+
+	Mostrar(p);
+
+
+
+
+}
