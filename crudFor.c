dce3a01fc1f031536b906826e22f382ac520af6e
Junior-Unix <sonjunior@live.com>
2025-03-01 18:18:31 -0300

    teste fifo

--- crudFor.c
+++ crudFor.c
@@ -1,20 +1,77 @@
-#include "stdio.h"
+#include <stdio.h>
 #include <string.h>
-int main() {
-    struct sPessoa *F;
-    Inic(&F);
-    int i;
+#include <stdlib.h>
 
-    for (i = 0; i < 1000; i++) {
-        printf("Inserir:\n");
-        Inserir(&F, i, "João");
-        printf("Listar:\n");
-        Listar(F);
-        printf("Apagar:\n");
-        Apagar(&F);
-        printf("Listar:\n");
-        Listar(F);
-    }
+struct sPessoa {
+	int Idade;
+	char Nome[20+1];
+	struct sPessoa *Prox;
+};
 
-    return 0;
+void Inic(struct sPessoa **Fila) {
+	*Fila = NULL;
 }
+
+/*Insere um novo registro no fim da Fila*/
+void Inserir(struct sPessoa **Fila, int Idade, char *Nome) {
+	if (*Fila == NULL) {
+		*Fila = (struct sPessoa *)malloc(sizeof(struct sPessoa));
+		if (*Fila == NULL)
+			return;
+		(*Fila)->Idade = Idade;
+		strcpy((*Fila)->Nome, Nome);
+		(*Fila)->Prox = NULL;
+	} else {
+		Inserir(&(*Fila)->Prox, Idade, Nome);
+	}
+}
+
+/*Apaga o primeiro elemento da Fila (se existir)*/
+void Apagar(struct sPessoa **Fila) {
+	struct sPessoa *Tmp = *Fila;
+	if (*Fila == NULL) /*Não existir elementos*/
+		return;
+	*Fila = (*Fila)->Prox;
+	free(Tmp);
+}
+
+/*Listar todos os elementos da Fila Recursivamente*/
+void Listar(struct sPessoa *Fila) {
+	if (Fila == NULL)
+		return; /*Não existe elementos.*/
+	printf("%d %d %s\n", (void *)Fila, Fila->Idade, Fila->Nome);
+	Listar(Fila->Prox); /*Lista os outros.*/
+}
+
+int main() {
+	struct sPessoa *F;
+	Inic(&F);
+	puts("Iniciar:");
+	Listar(F);
+	puts("Inserir:");
+    for (int i = 0; i < 1000; i++) {
+        if (i % 3 == 0) {
+            Inserir(&F, 10, "Tiago");
+        } else if (i % 3 == 1) {
+            Inserir(&F, 20, "Luiza");
+        } else {
+            Inserir(&F, 30, "Ana");
+        }
+    }
+	puts("Listar 3");
+	Listar(F);
+	Apagar(&F);
+	puts("Listar 2");
+	Listar(F);
+	Apagar(&F);
+	puts("Listar 1");
+	Listar(F);
+	Apagar(&F);
+	puts("Listar Nada");
+	Listar(F);
+	Apagar(&F);
+	puts("Listar Nada");
+	Listar(F);
+}
+ 
+
